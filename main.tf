cbfd9f0fbf4c05120e1e60f1ec9f3a6b783b0806
Morteza Baniadam <mortalbm@gmail.com>
2023-04-13 17:25:11 +0330

    added aws source

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 # Set the required provider and versions
 terraform {
   required_providers {
-    # We recommend pinning to the specific version of the Docker Provider you're using
-    # since new versions are released frequently
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.62.0"
     }
   }
 }
